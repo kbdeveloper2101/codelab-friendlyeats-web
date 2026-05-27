843bf834fe3f62b213291aeb262540c334e28fe0
kb.developer2101 <keita.ban2101@gmail.com>
2026-05-27 19:50:33 +0900

    Fix app.config.ts errors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-a5081","appId":"1:90648862113:web:f638b79175982f4d97bfe0","storageBucket":"friendlychat-a5081.firebasestorage.app","apiKey":"AIzaSyBXUUDOzjgryoPTy7kbFwsmv56NkbTo_hY","authDomain":"friendlychat-a5081.firebaseapp.com","messagingSenderId":"90648862113","projectNumber":"90648862113","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideFunctions(() => getFunctions()),
+    provideStorage(() => getStorage()),
+    provideMessaging(() => getMessaging()),
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-a5081","appId":"1:90648862113:web:f638b79175982f4d97bfe0","storageBucket":"friendlychat-a5081.firebasestorage.app","apiKey":"AIzaSyBXUUDOzjgryoPTy7kbFwsmv56NkbTo_hY","authDomain":"friendlychat-a5081.firebaseapp.com","messagingSenderId":"90648862113","version":"2"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
