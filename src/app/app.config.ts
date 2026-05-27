7b4288097ac0dc1ad4a77c95009cf33e57de12f3
kb.developer2101 <keita.ban2101@gmail.com>
2026-05-27 21:11:25 +0900

    Fix duplicate Firebase initialization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,19 +11,12 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes),
+    // ここで environment.ts に書かれた設定を使って1回だけ初期化します
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
-    provideFunctions(() => getFunctions()),
-    provideStorage(() => getStorage()),
+    provideFirestore(() => getFirestore()),
     provideMessaging(() => getMessaging()),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({
-      "projectId":"friendlychat-a5081",
-      "appId":"1:90648862113:web:f638b79175982f4d97bfe0",
-      "storageBucket":"friendlychat-a5081.firebasestorage.app",
-      "apiKey":"AIzaSyBXUUDOzjgryoPTy7kbFwsmv56NkbTo_hY",
-      "authDomain":"friendlychat-a5081.firebaseapp.com",
-      "messagingSenderId":"90648862113"
-    })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideStorage(() => getStorage())
   ],
 };
