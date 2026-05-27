4ae438c226e19d938479f50284881f181154c882
kb.developer2101 <keita.ban2101@gmail.com>
2026-05-27 21:33:23 +0900

    Add Firebase configuration to environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,18 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
+    //apiKey: 'API_KEY',
+    //authDomain: 'PROJECT_ID.firebaseapp.com',
+    //projectId: 'PROJECT_ID',
+    //storageBucket: 'PROJECT_ID.appspot.com',
+    //messagingSenderId: 'SENDER_ID',
+    //appId: 'APP_ID',
+    apiKey: "AIzaSyBXUUDOzjgryoPTy7kbFwsmv56NkbTo_hY",
+    authDomain: "friendlychat-a5081.firebaseapp.com",
+    projectId: "friendlychat-a5081",
+    storageBucket: "friendlychat-a5081.firebasestorage.app",
+    messagingSenderId: "90648862113",
+    appId: "1:90648862113:web:f638b79175982f4d97bfe0",
     databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    measurementId: 'G-MEASUREMENT_ID'
   },
 };
